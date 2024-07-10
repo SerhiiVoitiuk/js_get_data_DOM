3f9e23f4c9b414abd0e738b055a0778480b57c66
Sergio47ua <serhiivoitykappleid@gmail.com>
2024-07-10 14:47:30 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const populationElements = document.querySelectorAll("span.population");
+const totalPopulation = document.querySelectorAll("span.total-population")[0];
+const averagePopulation = document.querySelectorAll("span.average-population")[0];
+const population = [
+    ...populationElements
+].map((el)=>+el.textContent.replaceAll(",", ""));
+const total = population.reduce((prev, number)=>number + prev, 0);
+totalPopulation.textContent = total.toLocaleString("en-US");
+averagePopulation.textContent = (total / population.length).toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
